811256b17fff89e2bdda38cfce81f6ab2b13df4d
kanon07 <kanonc515046@gmail.com>
2019-06-10 18:21:23 +0900

    set grid

--- gnuplot_throughput.sh
+++ gnuplot_throughput.sh
@@ -12,6 +12,6 @@ echo $bbr
 echo $cubic
 
 #gnuplot -e "set xlabel 'time [s/10]'; set ylabel 'Throughput [Mbps]'; set xrange [0:300]; set yrange [0:2000]; plot '$bbr' using 0:7 with lines title 'TCP BBR'; replot '$cubic' using 0:7 with lines title 'CUBIC TCP'; pause -1"
-gnuplot -e "set xlabel 'time [s]'; set ylabel 'Throughput [Mbps]'; set yrange [0:2000]; plot '$bbr' using 0:7 with lines title 'TCP BBR'; replot '$cubic' using 0:7 with lines title 'CUBIC TCP'; set terminal png; set out '$1/number$2.png'; replot"
+gnuplot -e "set grid; set xlabel 'time [s]'; set ylabel 'Throughput [Mbps]'; set yrange [0:2000]; plot '$bbr' using 0:7 with lines title 'TCP BBR'; replot '$cubic' using 0:7 with lines title 'CUBIC TCP'; set terminal png; set out '$1/number$2.png'; replot"
 
 display $1/number$2.png
